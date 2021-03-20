f0147b60e46071ddc52f4f005a63bce333283e1c
unknown <benpgoff@gmail.com>
2021-03-20 18:46:09 -0500

    implemented panning, fixed errors stemming from usestate being asynchronous, and started implementing route printing

--- src/main/resources/static/maps-app/src/Route.js
+++ src/main/resources/static/maps-app/src/Route.js
@@ -7,8 +7,6 @@ import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 import axios from "axios";
 
-let pixToCoord;
-
 // global reference to the oldCanvas element
 let canvas;
 
@@ -30,20 +28,71 @@ const getPixelRatio = context => {
     return (window.devicePixelRatio || 1) / backingStore;
 };
 
+let startLat = 41.831311;
+let startLon = -71.406524;
+let endLat = 41.821395;
+let endLon = -71.396608;
+let clickCoordinate = [];
+let releaseCoordinate = [];
+
+function setStartLat(slat) {
+    startLat = parseFloat(slat)
+}
+
+function getStartLat() {
+    return parseFloat(startLat)
+}
+
+function setStartLon(slon) {
+    startLon = parseFloat(slon)
+}
+
+function getStartLon() {
+    return parseFloat(startLon)
+}
+
+function setEndLat(elat) {
+    endLat = parseFloat(elat)
+}
+
+function getEndLat() {
+    return parseFloat(endLat)
+}
+
+function setEndLon(elon) {
+    endLon = parseFloat(elon)
+}
+
+function getEndLon() {
+    return parseFloat(endLon)
+}
+
+function setClickCoordinate(click) {
+    clickCoordinate = click
+}
+
+function getClickCoordinate() {
+    return clickCoordinate
+}
+
+function setReleaseCoordinate(release) {
+    releaseCoordinate = release
+}
+
+function getReleaseCoordinate() {
+    return releaseCoordinate
+}
+
 
 
 function Route(props) {
-
-    const [startLat, setStartLat] = useState(0);
-    const [startLon, setStartLon] = useState(0);
-    const [endLat, setEndLat] = useState(0);
-    const [endLon, setEndLon] = useState(0);
+    const w = 600.0;
+    const h = 600.0;
+    let coordToPix = parseFloat((getStartLon() - getEndLon())/w);
     const [startLatBuffer, setStartLatBuffer] = useState(0);
     const [startLonBuffer, setStartLonBuffer] = useState(0);
     const [endLatBuffer, setEndLatBuffer] = useState(0);
     const [endLonBuffer, setEndLonBuffer] = useState(0);
-    const [clickCoordinate, setClickCoordinate] = useState([]);
-    const [releaseCoordinate, setReleaseCoordinate] = useState([]);
     const [zoomInFactor, setZoomInFactor] = useState(1);
     const [startCanvas, refreshCanvas] = useState("");
 //TODO: Fill in the ? with appropriate names/values for a route.
@@ -55,12 +104,13 @@ function Route(props) {
      * Makes an axios request.
      */
     const requestRoute = () => {
+        console.log(9 + " " + startLon)
         const toSend = {
             //TODO: Pass in the values for the data. Follow the format the route expects!
-            srclat : startLat,
-            srclong : startLon,
-            destlat : endLat,
-            destlong : endLon
+            srclat : getStartLat(),
+            srclong : getStartLon(),
+            destlat : getEndLat(),
+            destlong : getEndLon()
         };
 
         let config = {
@@ -86,43 +136,19 @@ function Route(props) {
 
             .catch(function (error) {
                 console.log(error);
-
             });
     }
 
-    //function Canvas(props) {
-        let w = 600;
-        let h = 600;
         let ref = useRef();
-        const firstUpdate = useRef(true);
+        const [firstRender, setFirstRender] = useState(true);
         useEffect(() => {
-            if (firstUpdate.current) {
-                // setEndLat(41.822895)
-                // setEndLon(-71.396608)
-                console.log(5)
-                // setStartLat(41.828693)
-                // setStartLon(-71.406524)
-
-                ReactDOM.unstable_batchedUpdates(() => {
-                    setStartLat(41.828693);
-                    setEndLat(41.822895);
-                    setEndLon(-71.396608);
-                    setStartLon(-71.406524);
-                    console.log(6)
-                    //requestRoute();
-                })
-                firstUpdate.current = false
-
+            if (firstRender) {
+                setFirstRender(false)
                 requestRoute()
-            } else {
-                console.log(7)
-                // endLat = parseFloat(props.routetorender[3]);
-                // endLon = parseFloat(props.routetorender[4]);
-                // startLat = parseFloat(props.routetorender[1]);
-                // startLon = parseFloat(props.routetorender[2]);
-
             }
             canvas = ref.current;
+            //http://www.petecorey.com/blog/2019/08/19/animating-a-canvas-with-react-hooks/
+            //smooths lines
             ctx = canvas.getContext("2d");
             let ratio = getPixelRatio(ctx);
             let width = getComputedStyle(canvas).getPropertyValue('width').slice(0, -2);
@@ -133,24 +159,14 @@ function Route(props) {
             canvas.style.height = `${height}px`;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.lineWidth = 1
-            pixToCoord = 50000;
 
-            //default
-            // endLat = 41.823876;
-            // endLon = -71.395963;
-            //
-            // startLat = (endLat - w/pixToCoord);
-            // startLon = (endLon - h/pixToCoord);
-
-            //with ways
-
-            console.log(endLat);
-            console.log(endLon);
-            console.log(startLat);
-            console.log(startLon);
+            console.log(getEndLat());
+            console.log(getEndLat());
+            console.log(getStartLat());
+            console.log(getStartLon());
 
 
-            let rawWaysData = route[3]//props.routetorender[0]
+            let rawWaysData = route[3]
             let listOfWays = [];
             if (rawWaysData != null) {
                 listOfWays = (rawWaysData + '').split(";");
@@ -162,10 +178,6 @@ function Route(props) {
             let startLonWay;
             let endLatWay;
             let endLonWay;
-
-
-            pixToCoord = ((startLon - endLon)/w);
-            h = (startLat - endLat)/pixToCoord;
 
             console.log("low" + listOfWays.length);
             for (let i=0; i<listOfWays.length; i++) {
@@ -179,27 +191,23 @@ function Route(props) {
                     console.log(w*(startLonWay - startLon)/(endLon - startLon));
                 }
                 ctx.beginPath();
-                ctx.moveTo(w*(startLonWay - startLon)/(endLon - startLon), 0 - h*(startLatWay - startLat)/(endLat - startLat));
-                ctx.lineTo(w*(endLonWay - startLon)/(endLon - startLon), 0 - h*(endLatWay - startLat)/(endLat - startLat));
+                ctx.moveTo(w*(startLonWay - getStartLon())/(getEndLon() - getStartLon()), h*(startLatWay - getStartLat())/(getEndLat() - getStartLat()));
+                ctx.lineTo(w*(endLonWay - getStartLon())/(getEndLon() - getStartLon()), h*(endLatWay - getStartLat())/(getEndLat() - getStartLat()));
                 //ctx.moveTo(w*(startLon - startLon)/(endLon - startLon), h-h*(startLat - startLat)/(endLat - startLat));
                 //ctx.lineTo(w*(endLon - startLon)/(endLon - startLon), h-h*(endLat - startLat)/(endLat - startLat));
                 ctx.strokeStyle = "red";
                 if (type === "" || type === "unclassified") {
                     ctx.strokeStyle = "blue";
+                } else {
+                    if (type === "path") {
+                        ctx.strokeStyle = "pink";
+                    }
                 }
                 ctx.stroke();
-
             }
             console.log("done")
 
         })
-        // return (
-        //
-        // );
-   // }
-
-
-
 
     const refreshButton = () => {
         setStartLat(startLatBuffer)
@@ -207,27 +215,60 @@ function Route(props) {
         setEndLat(endLatBuffer)
         setEndLon(endLonBuffer)
         requestRoute()
+        const toSend = {
+            //TODO: Pass in the values for the data. Follow the format the route expects!
+            srclat : getStartLat(),
+            srclong : getStartLon(),
+            destlat : getEndLat(),
+            destlong : getEndLon()
+        };
+
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+        //Install and import this!
+        //TODO: Fill in 1) location for request 2) your data 3) configuration
+        axios.post(
+            "http://localhost:4567/path",
+            toSend,
+            config
+        )
+            .then(response => {
+                console.log(response.data);
+                //TODO: Go to the Main.java in the server from the stencil, and find what variable you should put here.
+                //Note: It is very important that you understand how this is set up and why it works!
+                setRoute(route.concat(response.data["path"]));//console.log  the response.data["route"]
+            })
+
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
-    const clickedOnCanvas = (released) => {
+    const clickedOnCanvas = (clicked) => {
+        setClickCoordinate(clicked)
+    }
+
+    const releasedOnCanvas = (released) => {
         setReleaseCoordinate(released)
-        if(clickCoordinate === releaseCoordinate) {
+        if(getClickCoordinate() === getReleaseCoordinate()) {
             //route start/end
         } else {
             //panning
-            let dX = releaseCoordinate[0] - clickCoordinate[0]
-            let dY = releaseCoordinate[1] - clickCoordinate[1]
-            ReactDOM.unstable_batchedUpdates(() => {
-                setStartLat();
-                setEndLat();
-                setEndLon();
-                setStartLon();
+            let dX = (parseFloat(getReleaseCoordinate()[0]) - parseFloat(getClickCoordinate()[0]))*coordToPix
+            let dY = (parseFloat(getReleaseCoordinate()[1]) - parseFloat(getClickCoordinate()[1]))*coordToPix
+            console.log(releaseCoordinate[0])
+                setStartLat(getStartLat() - dY);
+                setEndLat(getEndLat() - dY);
+                setEndLon(getEndLon() + dX);
+                setStartLon(getStartLon() + dX);
                 requestRoute();
-            })
         }
     }
-
-
 
   const zoomIn = () => {
       setZoomInFactor(0.91);
@@ -242,40 +283,28 @@ function Route(props) {
   const setZoomCoords = () => {
       console.log(zoomInFactor);
 
-      let latFromCenter = Math.abs(Math.abs((startLat - (startLat + endLat)/2)) * zoomInFactor);
-      let lonFromCenter = Math.abs(Math.abs((endLon -  (endLon + startLon)/2)) * zoomInFactor);
+      let latFromCenter = Math.abs(Math.abs((getStartLat() - (getStartLat() + getEndLat())/2)) * zoomInFactor);
+      let lonFromCenter = Math.abs(Math.abs((getEndLon() -  (getEndLon() + getStartLon())/2)) * zoomInFactor);
 
-      //const oldStartLat = startLat;
-      //const oldStartLon = startLon;
-      //const oldEndLat = endLat;
-      //const oldEndLon = endLon;
+      const avgLat = ((getStartLat() + getEndLat())/2)
+      const avgLon = ((getStartLon() + getEndLon())/2)
 
-      //console.log(oldStartLat);
-      //console.log(oldStartLon);
-      //console.log(oldEndLat);
-      //console.log(oldEndLon);
-
-      const avgLat = ((startLat+endLat)/2)
-      const avgLon = ((startLon+endLon)/2)
-
-      const newStartLat = avgLat + (Math.abs(startLat - avgLat) * zoomInFactor);
+      const newStartLat = avgLat + (Math.abs(getStartLat() - avgLat) * zoomInFactor);
       console.log(newStartLat)
-      const newEndLat = avgLat - (Math.abs(endLat - avgLat) * zoomInFactor);
+      const newEndLat = avgLat - (Math.abs(getEndLat() - avgLat) * zoomInFactor);
       console.log(newEndLat);
-      const newEndLon = avgLon + (Math.abs(endLon - avgLon) * zoomInFactor);
+      const newEndLon = avgLon + (Math.abs(getEndLon() - avgLon) * zoomInFactor);
       console.log(newEndLon);
-      const newStartLon = avgLon - (Math.abs(startLon - avgLon) * zoomInFactor);
-      console.log((newStartLon));// + 3)/2);// - ((oldEndLon -  (oldEndLon + 3)/2) * 1));
+      const newStartLon = avgLon - (Math.abs(getStartLon() - avgLon) * zoomInFactor);
+      console.log((newStartLon));
       ReactDOM.unstable_batchedUpdates(() => {
           setStartLat(newStartLat);
           setEndLat(newEndLat);
           setEndLon(newEndLon);
           setStartLon(newStartLon);
-          requestRoute();
       })
+      requestRoute();
   }
-
-
 
   return (
     <div className="Route">
@@ -297,13 +326,11 @@ function Route(props) {
                 style={{ width: w, height: h }}
                 routetorender={props.routetorender}
                 onChange={(e) => props.onChange(e.target.value)}
-                //onMouseDown={(e) => props.onMouseDown([e.pageX, e.pageY])}
-                //onMouseUp={(e) => props.onMouseUp([e.pageX, e.pageY])}
+                onMouseDown={(e) => clickedOnCanvas([e.pageX, e.pageY])}
+                onMouseUp={(e) => releasedOnCanvas([e.pageX, e.pageY])}
             />
 
         </div>
-
-
     </div>
   );
 }
